81abbc5e0277a4d75ea7aacd72f9b5cb3f2e62e8
eclipzhl0rd <kuulersog@gmail.com>
2023-09-05 02:19:54 +0100

    thread component styling

--- src/components/Thread.jsx
+++ src/components/Thread.jsx
@@ -1,15 +1,33 @@
+"use client";
+
 import FormatDate from "@/lib/date";
+import { Avatar } from "@material-tailwind/react";
+import { LuMoreHorizontal } from "react-icons/lu";
 
 export default function Thread({ data }) {
   return (
     <>
       {data?.map((thread) => (
         <div key={thread.id} className="text-white">
-          <div className="">{thread.username}</div>
-          <div className="">{thread.content}</div>
-          <div className="">
-            <FormatDate dateString={thread.created_at} />
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-3">
+              <Avatar
+                className="h-9 w-9"
+                src="/av-placeholder.png"
+                alt="avatar"
+              />
+              <div className="">{thread.username}</div>
+            </div>
+            <div className="flex items-center gap-3">
+              <div className="text-sm text-gray-500">
+                {/* <FormatDate dateString={thread.created_at} /> */}3 h ago
+              </div>
+              <button className="text-lg">
+                <LuMoreHorizontal />
+              </button>
+            </div>
           </div>
+          <div className="pl-12">{thread.content}</div>
         </div>
       ))}
     </>
--- src/lib/date.js
+++ src/lib/date.js
@@ -1,6 +1,16 @@
-import { parseISO, format } from "date-fns";
+import { parseISO, formatDistance, subDays } from "date-fns";
 
 export default function FormatDate({ dateString }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+  const date = new Date(dateString);
+  const formattedDate = date.toLocaleDateString(
+    undefined,
+    Intl.DateTimeFormat("en-US")
+  );
+
+  const time = date.toLocaleTimeString([], {
+    hour: "numeric",
+    minute: "2-digit",
+  });
+
+  return `${time}, ${formattedDate}`;
 }
